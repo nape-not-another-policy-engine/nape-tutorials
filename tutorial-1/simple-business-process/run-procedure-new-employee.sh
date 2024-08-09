50978046db07a2fdd114a6a1e7db9309f03175d9
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2024-08-09 14:01:35 -0400

    updated directory names to be consistent with NAPE spec, add a bash file to execute new-employee process

---
+++ tutorial-1/simple-business-process/run-procedure-new-employee.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Cleaning up testing space"
+
+rm -rf nrn*
+
+echo "--- NAPE Collection Test 1 --- "
+
+nape collect start \
+    --subject "nrn:procedure:nape-tutorials/tutorial-1:new-employee" \
+    --subject-id 10387493 \
+    --procedure-link https://github.com/nape-not-another-policy-engine/nape-tutorials.git \
+    --procedure-directory tutorial-1/simple-business-process/new-employee \
+    --meta employee-name "Bill Bensing" \
+    --meta job-role "Software Engineer"
+
+echo "--- Collect the Background Check Evidence --- "
+
+nape collect evidence \
+    --control-activity "criminal-history" \
+    --file-path "raw-evidence/background-check.txt"
+
+echo "--- Execute the Assurance Reporting process based upon evidence collected --- "
+
+nape collect report
